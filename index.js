f3f00e9e0adcbe7fb25a09824c1b61c6bffa3127
Fabrice Lamant <flamant@shutterstock.com>
2024-07-09 12:14:41 +0100

    refactor

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
 
-const downloadSchedule = async (sport) => {
-  const cacheFile = `${__dirname}/cache/${sport}.html`;
+const downloadSchedule = async (sportKey) => {
+  const cacheFile = `${__dirname}/cache/${sportKey}.html`;
 
   if (!fs.existsSync(cacheFile)) {
-    const response = await fetch(`https://olympics.com/en/paris-2024/schedule/${sport}`);
+    const response = await fetch(`https://olympics.com/en/paris-2024/schedule/${sportKey}`);
     const content = await response.text();
     fs.writeFileSync(cacheFile, content);
   }
@@ -20,17 +20,19 @@ const downloadSchedule = async (sport) => {
 
 const getSportIcon = (sport) => {
   switch (sport.toLowerCase()) {
-    case "3x3-basketball": return "🏀3"
+    case "3x3-basketball": return "🏀③"
     case "basketball": return "🏀";
     case "football": return "⚽";
     case "handball": return "🤾";
     case "hockey": return "🏑";
+    case "rugby-sevens": return "🏉";
     case "volleyball": return "🏐";
+    case "water-polo": return "🤽";
     default: throw new Error(`No icon set for ${sport}`);
   }
 };
 
-const getFlag = (country) => {
+const getFlagIcon = (country) => {
   if (country.toLowerCase().startsWith("winner oqt")) {
     return "🏳️";
   }
@@ -47,10 +49,12 @@ const getFlag = (country) => {
     case "croatia": return "🇭🇷";
     case "denmark": return "🇩🇰";
     case "dominican republic": return "🇩🇴";
+    case "fiji": return "🇫🇯";
     case "france": return "🇫🇷";
     case "egypt": return "🇪🇬";
     case "germany": return "🇩🇪";
     case "great britain": return "🇬🇧";
+    case "greece": return "🇬🇷";
     case "guinea": return "🇬🇳";
     case "hungary": return "🇭🇺";
     case "india": return "🇮🇳";
@@ -64,6 +68,7 @@ const getFlag = (country) => {
     case "lithuania": return "🇱🇹";
     case "korea": return "🇰🇷";
     case "mali": return "🇲🇱";
+    case "montenegro": return "🇲🇪";
     case "morocco": return "🇲🇦";
     case "nigeria": return "🇳🇬";
     case "netherlands": return "🇳🇱";
@@ -72,111 +77,230 @@ const getFlag = (country) => {
     case "paraguay": return "🇵🇾";
     case "poland": return "🇵🇱";
     case "puerto rico": return "🇵🇷";
+    case "romania": return "🇷🇴";
     case "serbia": return "🇷🇸";
     case "south africa": return "🇿🇦";
     case "south sudan": return "🇸🇸";
     case "slovenia": return "🇸🇮";
+    case "samoa": return "🇼🇸";
     case "spain": return "🇪🇸";
     case "sweden": return "🇸🇪";
     case "türkiye": return "🇹🇷";
     case "ukraine": return "🇺🇦";
     case "united states": return "🇺🇸";
+    case "uruguay": return "🇺🇾";
     case "uzbekistan": return "🇺🇿";
     case "zambia": return "🇿🇲";
     default: throw new Error(`No flag set for ${country}`);
   }
 }
 
-const countryNameAndFlag = (name, flagFirst = false) => {
-  const flag = getFlag(name);
-  if (flagFirst) return `${flag} ${name}`;
-  return `${name} ${flag}`;
-}
+const SPORTS = [];
+const TEAMS = [];
+const EVENTS = [];
 
-const teams = [];
-const addTeamEvent = (team, event) => {
-  if (teams[team] == null) {
-    teams[team] = [];
-  }
-  teams[team].push(event);
-}
+const OUTPUT = [];
 
-const getTeamSport = async (sport) => {
-  const data = await downloadSchedule(sport);
-  const events = [];
-  const sportName = data.props.pageProps.page.template.properties.title;
+const generateCalendar = (title, key, events) => {
+  const lines = [];
+  lines.push(`BEGIN:VCALENDAR`);
+  lines.push(`VERSION:2.0`);
+  lines.push(`PRODID:-//fabrice404//olympics-calendar//${key}//EN`);
+  lines.push(`X-WR-CALNAME:${title}`);
+  lines.push(`NAME:${title}`);
 
-  data.props.pageProps.page.items.find(item => item.name === "scheduleWrapper").data.schedules.forEach(schedule => {
-    schedule.units.forEach(unit => {
-      let title = `${getSportIcon(sport)} ${unit.description}`;
-      let uid = `sport-${unit.startDateTimeUtc.replace(/[:-]/g, '')}`;
-
-      if (unit.match && unit.match.team1) {
-        title = `${getSportIcon(sport)} ${countryNameAndFlag(unit.match.team1.description)} - ${countryNameAndFlag(unit.match.team2.description, true)}`;
-        uid += `-${unit.match.team1.description.replace(/ /g, '-')}-${unit.match.team2.description.replace(/ /g, '-')}`;
-      }
-
-      const event = {
-        UID: uid,
-        DTSTAMP: unit.startDateTimeUtc.replace(/[:-]/g, ''),
-        DTSTART: unit.startDateTimeUtc.replace(/[:-]/g, ''),
-        DTEND: unit.endDateTimeUtc.replace(/[:-]/g, ''),
-        DESCRIPTION: unit.description,
-        SUMMARY: title,
-        LOCATION: schedule.venue ? schedule.venue.description : unit.venue.description,
-      }
-      events.push(event);
-      if (unit.match && unit.match.team1) {
-        addTeamEvent(unit.match.team1.description, event);
-      }
-
-      if (unit.match && unit.match.team2) {
-        addTeamEvent(unit.match.team2.description, event);
-      }
-    })
+  events.forEach((event) => {
+    lines.push(`BEGIN:VEVENT`);
+    lines.push(
+      ...Object.entries(event)
+        .filter(([key, value]) => !key.startsWith('_'))
+        .map(([key, value]) => `${key}:${value}`)
+    );
+    lines.push(`END:VEVENT`);
   });
 
-  const icalEvents = events.map(event => {
-    return `BEGIN:VEVENT\r\n${Object.entries(event).map(([key, value]) => `${key}:${value}`).join('\r\n')}\r\nEND:VEVENT`;
-  });
+  lines.push(`END:VCALENDAR`);
 
-  const name =
-    fs.writeFileSync(`docs/sport/${sport}.ics`, `BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//fabrice404//olympics-calendar//EN\r\nX-WR-CALNAME:${sportName} | Paris 2024\r\nNAME:${sportName} | Paris 2024\r\n${icalEvents.join('\r\n')}\r\nEND:VCALENDAR`);
+  const folder = `${__dirname}/docs/${key}.ics`.split('/').slice(0, -1).join('/');
+  fs.mkdirSync(folder, { recursive: true })
+  fs.writeFileSync(`${__dirname}/docs/${key}.ics`, lines.join('\r\n'));
+}
+
+const generateSportCalendar = (sportKey) => {
+  const sport = SPORTS.find((sport) => sport.key === sportKey);
+  const events = EVENTS.filter((event) => event._SPORT === sport.key);
+  const key = `${sportKey}/general`;
+  const title = `${sport.icon} ${sport.name} | Paris 2024`;
+
+  generateCalendar(title, key, events);
+  OUTPUT.push(`
+    <a href="${key}.ics" class="flex items-center pb-4 pt-8">
+      <div class="text-3xl">
+        ${sport.icon} ${sport.name}
+      </div>
+      <div class="bg-gray-200 hover:bg-blue-200 px-2 py-1 ml-4 rounded-lg text-base">
+        Full schedule
+      </div>
+    </a>
+  `);
+}
+
+const generateSportTeamCalendar = (sportKey, teamKey) => {
+  const sport = SPORTS.find((sport) => sport.key === sportKey);
+  const team = TEAMS.find((team) => team.key === teamKey);
+  const events = EVENTS.filter((event) => event._SPORT === sport.key && (event._TEAM1 === team.key || event._TEAM2 === team.key));
+  const key = `${sportKey}/${teamKey}`;
+  const title = `${team.icon} ${team.name} ${sport.name} | Paris 2024`;
+
+  generateCalendar(title, key, events);
+  OUTPUT.push(`
+    <li class="inline-block bg-gray-200 hover:bg-blue-200 px-2 py-1 mb-2 rounded-lg">
+      <a href="${key}.ics">
+        ${team.icon} ${team.key}
+      </a>
+    </li>
+  `);
 };
 
-const main = async () => {
-  const sports = [
-    '3x3-basketball',
-    'basketball',
-    'football',
-    'handball',
-    'hockey',
-    'volleyball'
-  ]
-  await Promise.all(sports.map(sport => getTeamSport(sport)));
+const generateTeamCalendar = (teamKey) => {
+  const team = TEAMS.find((team) => team.key === teamKey);
+  const events = EVENTS.filter((event) => event._TEAM1 === team.key || event._TEAM2 === team.key);
+  const key = `general/${teamKey}`;
+  const title = `${team.icon} ${team.name} | Paris 2024`;
 
-  Object.entries(teams).filter(([team, events]) => !team.startsWith("Winner")).forEach(([team, events]) => {
-    const icalEvents = events.map(event => {
-      return `BEGIN:VEVENT\r\n${Object.entries(event).map(([key, value]) => `${key}:${value}`).join('\r\n')}\r\nEND:VEVENT`;
+  generateCalendar(title, key, events);
+  OUTPUT.push(`
+    <li class="inline-block bg-gray-200 hover:bg-blue-200 px-2 py-1 mb-2 rounded-lg">
+      <a href="${key}.ics">
+        ${team.icon} ${team.key}
+      </a>
+    </li>
+  `);
+};
+
+const addSport = (name, key, icon) => {
+  if (!SPORTS.find((sport) => sport.key === key)) {
+    SPORTS.push({ name, key, icon, teams: [] });
+  }
+}
+
+const addTeam = (name, key, icon) => {
+  if (!TEAMS.find((team) => team.key === key)) {
+    TEAMS.push({ name, key, icon });
+  }
+};
+
+const addSportTeam = (sportKey, teamKey) => {
+  const sport = SPORTS.find((sport) => sport.key === sportKey);
+  if (sport && !sport.teams.includes(teamKey)) {
+    sport.teams.push(teamKey);
+  }
+}
+
+const isValidTeam = (team) => !team.toLowerCase().startsWith("winner oqt");
+
+const teamSport = async (sportKey) => {
+  const data = await downloadSchedule(sportKey);
+  const sportName = data.props.pageProps.page.template.properties.title;
+  const sportIcon = getSportIcon(sportKey);
+
+  addSport(sportName, sportKey, sportIcon);
+
+  data.props.pageProps.page.items.find((item) => item.name === "scheduleWrapper")
+    .data.schedules.forEach((schedule) => {
+      schedule.units.forEach(unit => {
+
+        const event = {
+          UID: `${sportKey}-${unit.startDateTimeUtc.replace(/[:-]/g, '')}`,
+          DTSTAMP: unit.startDateTimeUtc.replace(/[:-]/g, ''),
+          DTSTART: unit.startDateTimeUtc.replace(/[:-]/g, ''),
+          DTEND: unit.endDateTimeUtc.replace(/[:-]/g, ''),
+          DESCRIPTION: `${sportName} - ${unit.description}`,
+          SUMMARY: `${sportIcon} ${unit.description}`,
+          LOCATION: schedule.venue ? schedule.venue.description : unit.venue.description,
+          _SPORT: sportKey,
+        }
+
+        if (unit.match &&
+          unit.match.team1 && isValidTeam(unit.match.team1.description) &&
+          unit.match.team2 && isValidTeam(unit.match.team2.description)
+        ) {
+          const team1 = {
+            name: unit.match.team1.description,
+            key: unit.match.team1.teamCode,
+            icon: getFlagIcon(unit.match.team1.description),
+          };
+          addTeam(team1.name, team1.key, team1.icon)
+
+          const team2 = {
+            name: unit.match.team2.description,
+            key: unit.match.team2.teamCode,
+            icon: getFlagIcon(unit.match.team2.description),
+          };
+          addTeam(team2.name, team2.key, team2.icon)
+
+          event.UID += `-${team1.key}-${team2.key}`;
+          event.SUMMARY = `${sportIcon} ${team1.key} ${team1.icon} - ${team2.icon} ${team2.key}`;
+          event._TEAM1 = team1.key;
+          event._TEAM2 = team2.key;
+          addSportTeam(sportKey, team1.key);
+          addSportTeam(sportKey, team2.key);
+        }
+
+        EVENTS.push(event);
+
+      });
     });
-    const teamKey = team.toLowerCase().replace(/ /g, '-');
-    fs.writeFileSync(`docs/team/${teamKey}.ics`, `BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//fabrice404//olympics-calendar//EN\r\nX-WR-CALNAME:${team} | Paris 2024\r\nNAME:${team} | Paris 2024\r\n${icalEvents.join('\r\n')}\r\nEND:VCALENDAR`);
+}
+
+const teamSports = async () => {
+  await Promise.all(
+    [
+      '3x3-basketball',
+      'basketball',
+      'football',
+      'handball',
+      'hockey',
+      'rugby-sevens',
+      'volleyball',
+      'water-polo',
+    ]
+      .map((key) => teamSport(key))
+  );
+
+  SPORTS.sort((a, b) => a.name > b.name ? 1 : -1).forEach((sport) => {
+    const sportKey = sport.key;
+    generateSportCalendar(sportKey);
+    OUTPUT.push("<ul>")
+    sport.teams
+      .sort((a, b) => a > b ? 1 : -1)
+      .forEach((teamKey) => {
+        generateSportTeamCalendar(sportKey, teamKey);
+      });
+    OUTPUT.push("</ul>")
   });
 
+  OUTPUT.push(`<div class="text-3xl pb-4 pt-8">🌍 Teams</div>`);
+  TEAMS
+    .sort((a, b) => a.name > b.name ? 1 : -1)
+    .forEach((team) => {
+      generateTeamCalendar(team.key);
+    });
+}
+
+const main = async () => {
+  await teamSports();
 
   const template = fs.readFileSync(`${__dirname}/template.html`, 'utf-8');
-  const output = template
-    .replace('{{sports}}', sports.map(sport => `<li><a href="sport/${sport}.ics" class="text-blue-500">${getSportIcon(sport)} ${sport}</a></li>`).join('\n'))
-    .replace('{{teams}}', Object.keys(teams).sort().filter(team => !team.startsWith("Winner")).map(team => `<li><a href="team/${team.toLowerCase().replace(/ /g, '-')}.ics" class="text-blue-500">${countryNameAndFlag(team, true)}</a></li>`).join('\n'))
-
+  const output = template.replace('{{calendars}}', OUTPUT.join('\n'));
   fs.writeFileSync('docs/index.html', output);
 
-  const result = postcss([autoprefixer, tailwindcss])
+  postcss([autoprefixer, tailwindcss])
     .process(fs.readFileSync(`${__dirname}/template.css`, 'utf-8'), { from: 'template.css', to: 'docs/style.css' })
     .then((result) => {
       fs.writeFileSync('docs/style.css', result.css);
     })
-
 };
+
 
 main();
