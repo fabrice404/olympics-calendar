afa35437a2de657926cf70c63b4791fd71ee4c7f
Fabrice Lamant <flamant@shutterstock.com>
2024-08-05 09:14:57 +0200

    sort medals alphabetically if all are equals

--- src/calendar.ts
+++ src/calendar.ts
@@ -469,7 +469,7 @@ export class Calendar {
       if (a.bronze !== b.bronze) {
         return a.bronze < b.bronze ? 1 : -1;
       }
-      return 0;
+      return a.name > b.name ? 1 : -1;
     }).forEach((noc) => {
       content.push(`<div class="collapse collapse-arrow bg-gray-100 mb-1">`);
       content.push(`  <input type="radio" name="accordion">`);
